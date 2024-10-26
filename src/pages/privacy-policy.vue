9f92d2f058759eed38715f4732bd554508486c1a
baterdenebatsaikhan <baterdenebatsaikhan16@gmail.com>
2024-10-26 20:48:27 +0800

    create privacy-police

--- src/pages/privacy-policy.vue
+++ src/pages/privacy-policy.vue
@@ -1,111 +1,144 @@
 <template>
-  <div>
-    <div class="container mx-auto max-w-7xl	px-4 xl:px-12 pt-32 pb-60 text-black_color" v-if="getCurrentLanguage === 'mn_MN'">
-      <h1 class="text-2xl font-bold mb-4 text-center text-black_color">Нууцлалын бодлого</h1>
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">1. Оршил</h2>
-        <p class="text-base mb-4">Амжилт ERP-д тавтай морилно уу. Энэхүү "Нууцлалын бодлого" нь бид Амжилт
-          апп-ын хүрээнд хэрэглэгчийн ямар мэдээллийг хэрхэн боловсруулж ашигладаг тухай мэдээлэл өгнө.</p>
-      </div>
+  <section class="bg-gray-100 dark:bg-slate-900 xl:py-18 py-14 flex justify-center">
+    <div class="absolute top-0 inset-x-0 hidden sm:block">
+      <img src="/amjilthome/images/white-wave.svg" alt="svg" class="w-full -scale-x-100">
+    </div>
+    <div class="container max-w-4xl px-16 py-16 mx-auto aos-init aos-animate bg-white shadow " data-aos="fade-up" data-aos-duration="1500">
+      <h1 class="text-2xl font-bold mb-8 text-center text-black_color">Нууцлалын бодлого</h1>
+      <div class="text-justify space-y-3 text-sm">
+        <ul class="list-outside list-decimal space-y-6 leading-6">
+          <li>MCPC gr ирц бүртгэлийн аппликейшн нь хэрэглэгчийн мэдээллийг зөвхөн ирц бүртгэхэд ашиглана.
+          </li>
+          <li>Хэрэглэгчийн нэвтрэх нэр, нууц үгийн нууцлалыг зөвхөн хэрэглэгч өөрөө бүрэн хариуцах бөгөөд өөрийн нэвтрэх нэр, нууц кодыг ямар ч тохиолдолд бусдад дамжуулахгүй байх үүрэгтэй.
+          </li>
+          <li>
+            Хэрэглэгчийн хувийн мэдээллийг хэрэглэгчийн зөвшөөрөлгүйгээр гуравдагч этгээдэд хуваалцаж түгээхгүй ба "Нууцлалын бодлого"-д тусгаснаас өөр зорилгоор ашиглахгүй болно.
 
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">2. Бид ямар мэдээлэл цуглуулдаг вэ?</h2>
-        <p class="text-base mb-4">Амжилт апп-ыг хэрэглэхэд бид хэрэглэгчийн нэр, утасны дугаар, байрлалын мэдээллийг цуглуулдаг.
-          Энэ нь байгууллагын ирцийн бүртгэл буюу хэрэглэгчийн ажилд ирэх, гарах цагийн бүртгэл болон тухайн хэрэглэгчийн ажлын
-          цагийг тооцоолох зэрэг үйлчилгээг олгохын тулд хийгддэг юм. Мөн хэрэглэгч Амжилт апп-ын ямар онцлогуудыг хэрхэн
-          ашигладаг, систем ашиглаж байх явцад алдаа дутагдалтай тулгарсан эсэх зэрэг мэдээллийг бид цуглуулдаг. Энэ нь хэрэглэгчийн
-          хэрэгцээ, туршлага дээр үндэслэн апп-ыг сайжруулахад туслах юм.</p>
-      </div>
+          </li>
+          <li>
+            MCPC gr аппликейшн нь мэдээллийн аюулгүй байдлын зарчмыг баримтлан техник болон технологийн шийдлүүдээр хамгаалах боловч мобайл аппликейшнээс үл хамааран хэрэглэгч өөрийн буруутай үйлдлээс болж нэвтрэх нэр, нууц кодыг бусдад мэдэгдсэнээс үүссэн хохирол, хариуцлагыг хэрэглэгч өөрөө бүрэн хариуцна.
 
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">3. Бид мэдээллийг хэрхэн ашигладаг вэ?</h2>
-        <p class="text-base mb-4">Хэрэглэгчийн оруулсан мэдээлэл нь тухайн хэрэглэгчийн өмч бөгөөд “Амжилт ” ХХК нь хэрэглэгчийн
-          зөвшөөрөлгүйгээр дараах нөхцөлөөс бусад нөхцөлд ашиглахгүй болно.
-          <br>• Байгууллагын ирцийн бүртгэл, ажилд ирсэн явсан цагийг тодорхойлох
-          <br>• Систем сайжруулалтын хүрээнд хөгжүүлэлт хийх
-          <br>• Систем ашиглалтын зөвлөгөө, техникийн туслалцаа үзүүлэх</p>
-      </div>
-
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">4. Хэрэглэгчийн мэдээлийг хуваалцах & түгээх</h2>
-        <p class="text-base mb-4">Амжилт ERP нь таны хувийн мэдээллийг таны зөвшөөрөлгүйгээр гуравдагч этгээдэд хуваалцаж түгээхгүй.
-          Мөн "Нууцлалын бодлого"-д тусгаснаас өөр зорилгоор ашиглахгүй болно.</p>
-      </div>
-
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">5. Мэдээллийн аюулгүй байдлын хамгаалалт</h2>
-        <p class="text-base mb-4">Бид Амжилт апп-ын хэрэглэгчдийн мэдээллийг хамгаалахад аюулгүй байдлын зарчмыг баримтлан ажилладаг.
-          Гэвч хэрэглэгчийн хариуцлагагүй байдлын улмаас бусдад мэдээллээ дамжуулах, алдсанаас үүдсэн хохирлыг "Амжилт"
-          ХХК хариуцахгүй болно. </p>
-      </div>
-
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">6. Нууцлалын бодлогод өөрчлөлт оруулах</h2>
-        <p class="text-base mb-4">Бид энэхүү нууцлалын бодлогод өөрчлөлт оруулах бүрэн эрхтэй ба өөрчлөлт орсон тухай бүрд веб
-          хуудас дээр байршуулна.. Тиймээс та манай нууцлалын бодлогыг тогтмол шалгаж үзэхийг зөвлөж байна. Өөрчлөлт, тодруулга
-          нь веб хуудас дээр нийтлэгдсэнээс хойш хүчин төгөлдөр болно.</p>
-      </div>
-
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">7. Холбоо барих</h2>
-        <p class="text-base mb-4">Хэрвээ та манай Нууцлалын бодлого тухай асуух зүйлс, саналууд байгаа бол <span class="text-primary_color">info@tatatonga.com</span>
-          цахим шуудангийн хаяг, <span class="text-primary_color">+976 8080-9717</span> утасны дугаараар холбоо барина уу.</p>
+          </li>
+          <li>
+            “MCPC gr”  ХХК нь энэхүү нууцлалын бодлогод өөрчлөлт оруулах бүрэн эрхтэй ба өөрчлөлт орсон тухай бүрд
+            <a href="https://erp.mcpc.mn/privacy-policy" target="_blank" class="font-semibold">https://erp.mcpc.mn/privacy-policy</a> байршина.
+            Тиймээс нууцлалын бодлогыг тогтмол шалгахыг зөвлөж байна. Өөрчлөлт, тодруулга нь
+            <a href="https://erp.mcpc.mn/privacy-policy" target="_blank" class="font-semibold">https://erp.mcpc.mn/privacy-policy</a> дээр нийтлэгдсэнээс хойш хүчин төгөлдөр байна.
+          </li>
+<!--          <li>-->
+<!--            Нууцлалын бодлогын талаар асууж тодруулах, нэмэлт саналууд гарвал <a href="" target="_blank">info@amjilt.mn</a> цахим шуудангийн хаяг, +976 9901-9692 утасны дугаараар холбоо барина уу.-->
+<!--          </li>-->
+        </ul>
       </div>
     </div>
-    <div class="container mx-auto max-w-7xl	px-4 xl:px-12 pt-32 pb-60 text-black_color" v-else>
-      <h1 class="text-2xl font-bold mb-4 text-center text-black_color">Privacy policy</h1>
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">1. Introduction</h2>
-        <p class="text-base mb-4">Welcome to Amjilt ERP. This Privacy Policy describes how we collect, use, process,
-          and disclose your information, including personal information, in conjunction with your access to and use of
-          the Amjilt App.</p>
-      </div>
+  </section>
+<!--  <div>-->
+<!--    <div class="container mx-auto max-w-7xl	px-4 xl:px-12 pt-32 pb-60 text-black_color" v-if="getCurrentLanguage === 'mn_MN'">-->
+<!--      <h1 class="text-2xl font-bold mb-4 text-center text-black_color">Нууцлалын бодлого</h1>-->
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">1. Оршил</h2>-->
+<!--        <p class="text-base mb-4">Амжилт ERP-д тавтай морилно уу. Энэхүү "Нууцлалын бодлого" нь бид Амжилт-->
+<!--          апп-ын хүрээнд хэрэглэгчийн ямар мэдээллийг хэрхэн боловсруулж ашигладаг тухай мэдээлэл өгнө.</p>-->
+<!--      </div>-->
 
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">2. Information We Collect</h2>
-        <p class="text-base mb-4">When you use the Amjilt app, we collect various types of information. Personal
-          information such as your name, phone number, and profile picture is collected when you create an account or
-          update your profile. In addition, we collect data about your location when you use the app to register your
-          attendance at your workplace. This is done via a Bluetooth beacon at your workplace, which connects to your
-          device when you are within range. We also collect data about the times you arrive at and leave work, which
-          forms part of your attendance record. This information is used to provide services related to your employment,
-          such as calculating working hours and managing payroll. Furthermore, we collect usage data, such as how often
-          you log into the app, which features you use, and any issues or errors you encounter. This helps us to improve
-          the app and provide a better user experience.</p>
-      </div>
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">2. Бид ямар мэдээлэл цуглуулдаг вэ?</h2>-->
+<!--        <p class="text-base mb-4">Амжилт апп-ыг хэрэглэхэд бид хэрэглэгчийн нэр, утасны дугаар, байрлалын мэдээллийг цуглуулдаг.-->
+<!--          Энэ нь байгууллагын ирцийн бүртгэл буюу хэрэглэгчийн ажилд ирэх, гарах цагийн бүртгэл болон тухайн хэрэглэгчийн ажлын-->
+<!--          цагийг тооцоолох зэрэг үйлчилгээг олгохын тулд хийгддэг юм. Мөн хэрэглэгч Амжилт апп-ын ямар онцлогуудыг хэрхэн-->
+<!--          ашигладаг, систем ашиглаж байх явцад алдаа дутагдалтай тулгарсан эсэх зэрэг мэдээллийг бид цуглуулдаг. Энэ нь хэрэглэгчийн-->
+<!--          хэрэгцээ, туршлага дээр үндэслэн апп-ыг сайжруулахад туслах юм.</p>-->
+<!--      </div>-->
 
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">3. How We Use Information</h2>
-        <p class="text-base mb-4">We use the information we collect to provide, improve, and develop the Amjilt app, to
-          create and maintain a safer environment and comply with our legal obligations.</p>
-      </div>
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">3. Бид мэдээллийг хэрхэн ашигладаг вэ?</h2>-->
+<!--        <p class="text-base mb-4">Хэрэглэгчийн оруулсан мэдээлэл нь тухайн хэрэглэгчийн өмч бөгөөд “Амжилт ” ХХК нь хэрэглэгчийн-->
+<!--          зөвшөөрөлгүйгээр дараах нөхцөлөөс бусад нөхцөлд ашиглахгүй болно.-->
+<!--          <br>• Байгууллагын ирцийн бүртгэл, ажилд ирсэн явсан цагийг тодорхойлох-->
+<!--          <br>• Систем сайжруулалтын хүрээнд хөгжүүлэлт хийх-->
+<!--          <br>• Систем ашиглалтын зөвлөгөө, техникийн туслалцаа үзүүлэх</p>-->
+<!--      </div>-->
 
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">4. Sharing & Disclosure</h2>
-        <p class="text-base mb-4">Amjilt ERP does not share your personal information except as approved by you or as
-          described herein.</p>
-      </div>
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">4. Хэрэглэгчийн мэдээлийг хуваалцах & түгээх</h2>-->
+<!--        <p class="text-base mb-4">Амжилт ERP нь таны хувийн мэдээллийг таны зөвшөөрөлгүйгээр гуравдагч этгээдэд хуваалцаж түгээхгүй.-->
+<!--          Мөн "Нууцлалын бодлого"-д тусгаснаас өөр зорилгоор ашиглахгүй болно.</p>-->
+<!--      </div>-->
 
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">5. Data Security</h2>
-        <p class="text-base mb-4">We are committed to protecting the data of the Amjilt app’s users. Even though we
-          take reasonable precautions to protect your data, no security measures can be 100% secure, and we cannot
-          guarantee the security of your data.</p>
-      </div>
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">5. Мэдээллийн аюулгүй байдлын хамгаалалт</h2>-->
+<!--        <p class="text-base mb-4">Бид Амжилт апп-ын хэрэглэгчдийн мэдээллийг хамгаалахад аюулгүй байдлын зарчмыг баримтлан ажилладаг.-->
+<!--          Гэвч хэрэглэгчийн хариуцлагагүй байдлын улмаас бусдад мэдээллээ дамжуулах, алдсанаас үүдсэн хохирлыг "Амжилт"-->
+<!--          ХХК хариуцахгүй болно. </p>-->
+<!--      </div>-->
 
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">6. Changes to This Privacy Policy</h2>
-        <p class="text-base mb-4">We reserve the right to modify this privacy policy at any time, so please review it
-          frequently. Changes and clarifications will take effect immediately upon their posting on the website.</p>
-      </div>
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">6. Нууцлалын бодлогод өөрчлөлт оруулах</h2>-->
+<!--        <p class="text-base mb-4">Бид энэхүү нууцлалын бодлогод өөрчлөлт оруулах бүрэн эрхтэй ба өөрчлөлт орсон тухай бүрд веб-->
+<!--          хуудас дээр байршуулна.. Тиймээс та манай нууцлалын бодлогыг тогтмол шалгаж үзэхийг зөвлөж байна. Өөрчлөлт, тодруулга-->
+<!--          нь веб хуудас дээр нийтлэгдсэнээс хойш хүчин төгөлдөр болно.</p>-->
+<!--      </div>-->
 
-      <div class="mb-8">
-        <h2 class="text-lg font-bold mb-2 text-black_color">7. Contact Us</h2>
-        <p class="text-base mb-4">If you have any questions or suggestions about our Privacy Policy, do not hesitate to
-          contact us at <span class="text-primary_color">info@tatatonga.com</span> or <span class="text-primary_color">+976 8080-9717</span>.</p>
-      </div>
-    </div>
-  </div>
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">7. Холбоо барих</h2>-->
+<!--        <p class="text-base mb-4">Хэрвээ та манай Нууцлалын бодлого тухай асуух зүйлс, саналууд байгаа бол <span class="text-primary_color">info@tatatonga.com</span>-->
+<!--          цахим шуудангийн хаяг, <span class="text-primary_color">+976 8080-9717</span> утасны дугаараар холбоо барина уу.</p>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div class="container mx-auto max-w-7xl	px-4 xl:px-12 pt-32 pb-60 text-black_color" v-else>-->
+<!--      <h1 class="text-2xl font-bold mb-4 text-center text-black_color">Privacy policy</h1>-->
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">1. Introduction</h2>-->
+<!--        <p class="text-base mb-4">Welcome to Amjilt ERP. This Privacy Policy describes how we collect, use, process,-->
+<!--          and disclose your information, including personal information, in conjunction with your access to and use of-->
+<!--          the Amjilt App.</p>-->
+<!--      </div>-->
+
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">2. Information We Collect</h2>-->
+<!--        <p class="text-base mb-4">When you use the Amjilt app, we collect various types of information. Personal-->
+<!--          information such as your name, phone number, and profile picture is collected when you create an account or-->
+<!--          update your profile. In addition, we collect data about your location when you use the app to register your-->
+<!--          attendance at your workplace. This is done via a Bluetooth beacon at your workplace, which connects to your-->
+<!--          device when you are within range. We also collect data about the times you arrive at and leave work, which-->
+<!--          forms part of your attendance record. This information is used to provide services related to your employment,-->
+<!--          such as calculating working hours and managing payroll. Furthermore, we collect usage data, such as how often-->
+<!--          you log into the app, which features you use, and any issues or errors you encounter. This helps us to improve-->
+<!--          the app and provide a better user experience.</p>-->
+<!--      </div>-->
+
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">3. How We Use Information</h2>-->
+<!--        <p class="text-base mb-4">We use the information we collect to provide, improve, and develop the Amjilt app, to-->
+<!--          create and maintain a safer environment and comply with our legal obligations.</p>-->
+<!--      </div>-->
+
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">4. Sharing & Disclosure</h2>-->
+<!--        <p class="text-base mb-4">Amjilt ERP does not share your personal information except as approved by you or as-->
+<!--          described herein.</p>-->
+<!--      </div>-->
+
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">5. Data Security</h2>-->
+<!--        <p class="text-base mb-4">We are committed to protecting the data of the Amjilt app’s users. Even though we-->
+<!--          take reasonable precautions to protect your data, no security measures can be 100% secure, and we cannot-->
+<!--          guarantee the security of your data.</p>-->
+<!--      </div>-->
+
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">6. Changes to This Privacy Policy</h2>-->
+<!--        <p class="text-base mb-4">We reserve the right to modify this privacy policy at any time, so please review it-->
+<!--          frequently. Changes and clarifications will take effect immediately upon their posting on the website.</p>-->
+<!--      </div>-->
+
+<!--      <div class="mb-8">-->
+<!--        <h2 class="text-lg font-bold mb-2 text-black_color">7. Contact Us</h2>-->
+<!--        <p class="text-base mb-4">If you have any questions or suggestions about our Privacy Policy, do not hesitate to-->
+<!--          contact us at <span class="text-primary_color">info@tatatonga.com</span> or <span class="text-primary_color">+976 8080-9717</span>.</p>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
 </template>
 
 <script>
